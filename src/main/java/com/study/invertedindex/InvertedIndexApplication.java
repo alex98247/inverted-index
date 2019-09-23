be9da97001fd6ee549bead75e6580ab1199cf5d9
Alex <alexmir2015@yandex.ru>
2019-09-23 15:57:27 +0300

    helloWorld

---
+++ src/main/java/com/study/invertedindex/InvertedIndexApplication.java
@@ -0,0 +1,13 @@
+package com.study.invertedindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertedIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvertedIndexApplication.class, args);
+	}
+
+}
